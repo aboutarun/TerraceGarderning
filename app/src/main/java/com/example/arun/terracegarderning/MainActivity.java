34f1e448135d938c0be76a3ac4596cf3ab840369
lokivarman5 <30750553+lokivarman5@users.noreply.github.com>
2017-09-15 06:54:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/arun/terracegarderning/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.arun.terracegarderning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+package com.androidbegin.splashtutorial;
+
+        import android.os.Bundle;
+        import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the view from activity_main.xml
+        setContentView(R.layout.activity_main);
+    }
+
+}
